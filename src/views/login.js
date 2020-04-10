a3f106648c5470f07bd1265ed651d45d7d93e5c1
Anderson <andersonribeiro.si@hotmail.com>
2020-04-10 13:47:47 -0300

    Adding state and behavior to the login component

--- src/views/login.js
+++ src/views/login.js
@@ -3,6 +3,17 @@ import Card from '../components/card'
 import FormGroup from '../components/form-group'
 
 class Login extends React.Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    entrar = () => {
+        console.log('Email: ' + this.state.email);
+        console.log('Senha: ' + this.state.password);
+    }
+
     render() {
         return (
             <form>
@@ -18,18 +29,22 @@ class Login extends React.Component {
                                                 <fieldset>
                                                     <FormGroup label="Email: *" htmlFor="inputEmail">
                                                         <input type="email"
+                                                            value={this.state.email}
+                                                            onChange={e => this.setState({ email: e.target.value })}
                                                             id="inputEmail"
                                                             placeholder="Digite seu email"
                                                             className="form-control" />
                                                     </FormGroup>
                                                     <FormGroup label="Senha: *" htmlFor="inputPassword">
                                                         <input type="password"
+                                                            value={this.state.password}
+                                                            onChange={e => this.setState({ password: e.target.value })}
                                                             id="inputPassword"
                                                             placeholder="Digite seu email"
                                                             className="form-control" />
                                                     </FormGroup>
-                                                    <button type="button" className="btn btn-success mr-3 mt-3">Entrar</button>
-                                                    <button type="button" className="btn btn-warning mt-3">Cadastrar</button>
+                                                    <button type="button" className="btn btn-success mr-3 mt-3" onClick={this.entrar}>Entrar</button>
+                                                    <button type="button" className="btn btn-danger mt-3">Cadastrar</button>
                                                 </fieldset>
                                             </div>
                                         </div>
